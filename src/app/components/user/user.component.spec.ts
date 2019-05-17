0739d6d90a9df0dc8ed2e7ea20e06a08c97d092d
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2019-05-17 19:05:46 +0200

    Added test for user component

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -4,11 +4,12 @@ import { UserComponent } from './user.component';
 import {UserDetailComponent} from "../user-detail/user-detail.component";
 import {UserListComponent} from "../user-list/user-list.component";
 import {RouterTestingModule} from "@angular/router/testing";
-import {BrowserModule} from "@angular/platform-browser";
+import {BrowserModule, By} from "@angular/platform-browser";
 import {NgxPaginationModule} from "ngx-pagination";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {SearchService} from "../../../../../../Cloudera/github-search/src/app/services/search/search.service";
 import {HttpTestingController} from "@angular/common/http/testing";
+import {Issue} from "../../../../../../Cloudera/github-search/src/app/models/issue/issue";
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -36,5 +37,17 @@ describe('UserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the user information properly', () => {
+    component.firstName = 'firstName';
+    component.lastName = 'lastName';
+    component.avatarImage = 'avatarImage';
+    component.id = 1;
+
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('.navbar h3')).nativeElement.innerText).toEqual('firstName lastName');
+    expect(fixture.debugElement.query(By.css('img')).nativeElement.getAttribute('src')).toEqual('avatarImage');
   });
 });
