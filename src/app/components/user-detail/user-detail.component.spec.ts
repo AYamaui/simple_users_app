a9d0e2f2395a07c1fe224471d621334fa6deac58
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2019-05-17 19:24:26 +0200

    Added tests for user detail component

--- src/app/components/user-detail/user-detail.component.spec.ts
+++ src/app/components/user-detail/user-detail.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailComponent } from './user-detail.component';
 import {RouterTestingModule} from "@angular/router/testing";
-import {BrowserModule} from "@angular/platform-browser";
+import {BrowserModule, By} from "@angular/platform-browser";
 import {AppRoutingModule} from "../../app-routing.module";
 import {NgxPaginationModule} from "ngx-pagination";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
@@ -10,6 +10,7 @@ import {UserListComponent} from "../user-list/user-list.component";
 import {SearchService} from "../../../../../../Cloudera/github-search/src/app/services/search/search.service";
 import {HttpTestingController} from "@angular/common/http/testing";
 import {UserComponent} from "../user/user.component";
+import {User} from "../../models/user";
 
 describe('UserDetailComponent', () => {
   let component: UserDetailComponent;
@@ -37,5 +38,23 @@ describe('UserDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the user information properly', () => {
+    component.user = new User(
+      'avatarImage',
+      'firstName',
+      'lastName',
+      1,
+      'email'
+    );
+
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('h2')).nativeElement.innerText).toEqual('firstName lastName');
+    expect(fixture.debugElement.query(By.css('img')).nativeElement.getAttribute('src')).toEqual('avatarImage');
+    expect(fixture.debugElement.query(By.css('.email')).nativeElement.innerText).toEqual('Email: email');
+    expect(fixture.debugElement.query(By.css('.id')).nativeElement.innerText).toEqual('Id: 1');
+    expect(fixture.debugElement.query(By.css('button')).nativeElement).toBeDefined();
   });
 });
