90b726b78c636639eb5077d41483a70e5edc14c8
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2019-05-19 16:08:46 +0200

    Added more unit tests for the data service

--- src/app/services/data/data-service.service.spec.ts
+++ src/app/services/data/data-service.service.spec.ts
@@ -1,25 +1,22 @@
 import {async, TestBed} from '@angular/core/testing';
 
 import { DataServiceService } from './data-service.service';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {User} from "../../models/user";
 
 describe('DataServiceService', () => {
 
-  let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
   let service: DataServiceService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ DataServiceService, HttpTestingController, HttpClient ],
+      providers: [ DataServiceService ],
       imports: [
-        HttpClientTestingModule,
-        HttpClientModule
+        HttpClientTestingModule
       ]
   });
 
-    httpClient = TestBed.get(HttpClient);
     httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.get(DataServiceService);
   });
@@ -33,7 +30,7 @@ describe('DataServiceService', () => {
   it('should receive the basic information when the request is sent',
     () => {
 
-      const mockUsersResponse  : object = {
+      const mockHttpResponse  : object = {
         "page": 1,
         "per_page": "3",
         "title": "title",
@@ -44,20 +41,35 @@ describe('DataServiceService', () => {
             "email":"george.bluth@reqres.in",
             "first_name":"George",
             "last_name":"Bluth",
-            "avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"}
+            "avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+          }
         ]
       };
+      const mockUsersResponse: User[] = [
+          new User(
+            'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg',
+            'George',
+            'Bluth',
+            1,
+            'george.bluth@reqres.in'
+          )
+      ];
 
       // HTTP request
       service.getUsers( 1).subscribe( ( resp: object ) => {
-        expect(resp).toEqual(mockUsersResponse);
+        expect(resp).toEqual(mockHttpResponse);
       });
+
+      const req = httpTestingController.expectOne('https://reqres.in/api/users?page=1');
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockUsersResponse);
+      httpTestingController.verify();
     }
   );
 
   // Tests if the HTTP request retrieves the issues correctly
-  it('should receive a single user information when the request is sent', async (() => {
-    const mockUserResponse  : object = {
+  it('should receive a single user information when the request is sent', () => {
+    const mockHttpResponse: object = {
       "data": [
         {
           "id": 1,
@@ -69,10 +81,23 @@ describe('DataServiceService', () => {
       ]
     };
 
+    const mockUserResponse: User = new User(
+      "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg",
+      "George",
+      "Bluth",
+      1,
+      "george.bluth@reqres.in"
+    );
+
     // HTTP GET request
     service.getUser( 1 ).subscribe( ( resp: object ) => {
-      expect(resp).toEqual(mockUserResponse);
+      expect(resp).toEqual(mockHttpResponse);
     });
-  }));
+
+    const req = httpTestingController.expectOne('https://reqres.in/api/users/1');
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockUserResponse);
+    httpTestingController.verify();
+  });
 
 });
